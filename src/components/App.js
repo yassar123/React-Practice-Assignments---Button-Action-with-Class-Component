20fb3855f397c5372322b5d7442343757ada46ea
yassar123 <yassarali1999@gmail.com>
2021-05-17 20:45:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,22 @@ import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
-		super(props);
-	};
-
+		super(props)
+		this.state ={
+			message:''
+		}
+	}
+	changeMessage() {
+		this.setState({
+			message: " Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+		})
+	}
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={() => this.changeMessage()} >click</button>
+				<p id="para">{this.state.message}</p>
     		</div>
     	);
     }
