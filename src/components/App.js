a584195a63678b2b938727707ed42dbcab4cd2cb
yassar123 <yassarali1999@gmail.com>
2021-05-17 21:08:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,24 @@ import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
-		super(props)
-		this.state ={
-			message:''
-		}
-	}
-	changeMessage() {
-		this.setState({
-			message: "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-		})
-	}
+		super(props);
+		this.state = {
+			isShow: false
+		  }
+		this.createText = this.createText.bind(this);
+		
+	};
+
+	createText() {
+		this.setState({ isShow: true }) 
+	  }
+
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
-				<button id="click" onClick={() => this.changeMessage()} >click</button>
-				<p id="para">{this.state.message}</p>
+				<button id="para" onClick={this.createText}>Click</button>
+				{this.state.isShow && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     		</div>
     	);
     }
